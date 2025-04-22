eb8bb4bdb09078c98d4808cf5db34a4da5143a92
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-22 14:57:23 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -55,6 +55,12 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+
+        #こうかとんRectと爆弾Rectが重なっていたら
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
